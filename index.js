f1232b83aac1af6f998e698f099a3a35b092308b
OKBEntropy <cqpro.22@gmail.com>
2025-11-07 16:14:47 +0100

    Exercice-2-MsgDynaOk

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-document.getElementById("formulaire").addEventListener("submit", function(event) {
-  event.preventDefault(); // Empêche le rechargement de la page
+    document.getElementById("formulaire").addEventListener("submit", function(event) {
+    event.preventDefault(); // Empêche le rechargement de la page
 
-  const nom = document.getElementById("nom").value.trim();
-  const prenom = document.getElementById("prenom").value.trim();
-  const email = document.getElementById("email").value.trim();
-  const message = document.getElementById("message");
+    const nom = document.getElementById("nom").value.trim();
+    const prenom = document.getElementById("prenom").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const message = document.getElementById("message");
 
-  if (nom && prenom && email) {
-    message.textContent = `Merci ${prenom} ${nom}, votre formulaire a été envoyé !`;
+    (nom && prenom && email) {
+    message.textContent = `${prenom} ${nom}, Votre demande est bien envoyée !`;
     message.style.color = "green";
     this.reset(); // Réinitialise le formulaire
-  } else {
-    message.textContent = "Veuillez remplir tous les champs.";
-    message.style.color = "red";
-  }
+  } 
 });
